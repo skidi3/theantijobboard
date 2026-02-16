aa42f3dcaa6cdbae665d6f4a1cec9f6c02a89d0e
skidi3 <nikhilkatta10@gmail.com>
2026-02-16 17:52:37 +0530

    configured posthog for analytics

---
+++ src/instrumentation-client.ts
@@ -0,0 +1,7 @@
+import posthog from 'posthog-js'
+
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    capture_pageview: true,
+    capture_pageleave: true,
+})
